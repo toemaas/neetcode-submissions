d1e5c84aeeef242241b9057f21650be00ff00f20
Thomas Nguyen <96025679+toemaas@users.noreply.github.com>
2026-09-02 08:19:41 -0400

    Add: non-cyclical-number - submission-1

---
+++ Data Structures & Algorithms/non-cyclical-number/submission-1.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        hashset = set()
+
+        while n != 1:
+            curSum = 0
+            digit = n
+            while digit > 0:
+                curSum += (digit % 10) ** 2
+                digit //= 10
+            if curSum in hashset:
+                return False
+            hashset.add(curSum)
+            n = curSum
+        return True
+
+        
